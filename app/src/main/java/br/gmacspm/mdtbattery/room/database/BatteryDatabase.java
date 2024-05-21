e6df37b8a40175ed9a7e7779ffdbb757a22ea0a3
GmacSpm <gimacbr@gmail.com>
2024-05-21 16:14:43 -0300

    Renamed some comments

--- app/src/main/java/br/gmacspm/mdtbattery/room/database/BatteryDatabase.java
+++ app/src/main/java/br/gmacspm/mdtbattery/room/database/BatteryDatabase.java
@@ -12,19 +12,18 @@ import androidx.room.RoomDatabase;
 // Annotation para nossas entities da database e db version.
 @Database(entities = {UsageModel.class}, version = 3, exportSchema = false)
 public abstract class BatteryDatabase extends RoomDatabase {
-
-    // Criar instancia
+    // Criar instância
     private static BatteryDatabase database;
 
     // Definir nome para o database
     private static final String DATABASE_NAME = "battery_database";
 
-    // Pegar instance para nosso database
+    // Pegar instância para nosso database
     public static synchronized BatteryDatabase getInstance(Context context) {
 
-        // Verificar se instancia já existe
+        // Verificar se instância já existe
         if (database == null) {
-            // Criar nova instancia se null.
+            // Criar nova instância se null.
             database = Room.databaseBuilder(
                             context.getApplicationContext(),
                             BatteryDatabase.class, DATABASE_NAME
