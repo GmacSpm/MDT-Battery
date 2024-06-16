0efc28acd43f65ada8fa14c497c98da3907ab58d
GmacSpm <gimacbr@gmail.com>
2024-06-16 05:29:24 -0300

    Test TimeConverter

--- app/src/test/java/br/gmacspm/mdtbattery/ExampleUnitTest.java
+++ app/src/test/java/br/gmacspm/mdtbattery/ExampleUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
@@ -19,4 +20,19 @@ import br.gmacspm.mdtbattery.utils.TimeConverter;
 
 
 public class ExampleUnitTest {
+    @Test
+    public void isTimeConvertOK(){
+        assertEquals("00m01s", TimeConverter.getHumanTime(1000, false));
+        assertEquals("00m59s", TimeConverter.getHumanTime(59999, false));
+
+        assertEquals("01m00s", TimeConverter.getHumanTime(60000, false));
+        assertEquals("59m59s", TimeConverter.getHumanTime(3599999, false));
+
+        assertEquals("01h00m", TimeConverter.getHumanTime(3600000, false));
+        assertEquals("23h59m", TimeConverter.getHumanTime(86399999, false));
+
+        assertEquals("01d00h", TimeConverter.getHumanTime(86400000, false));
+        assertEquals("01d01h", TimeConverter.getHumanTime(90000000, false));
+
+    }
 }
